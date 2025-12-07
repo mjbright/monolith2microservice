ca85394b32d1bdf825b76e9bc9636f86d1668db1
Michael Bright <1880109+mjbright@users.noreply.github.com>
2025-12-07 22:21:25 +0100

    Expose port on localhost & VM ipaddress

--- 2.Pods_Namespaces/docker_run.sh
+++ 2.Pods_Namespaces/docker_run.sh
@@ -9,9 +9,15 @@ DOCKER_RM_F() {
 DOCKER_RUN() {
     MODE=$1; shift
 
-    docker run -d --name ${MODE}1 mjbright/flask-${MODE}:v1
-    docker run -d --name ${MODE}2 mjbright/flask-${MODE}:v2
-    docker run -d --name ${MODE}3 mjbright/flask-${MODE}:v3
+    PORT1=$1
+    docker run -p $1:5000 -d --name ${MODE}1 mjbright/flask-${MODE}:v1
+    shift
+    PORT2=$1
+    docker run -p $1:5000 -d --name ${MODE}2 mjbright/flask-${MODE}:v2
+    shift
+    PORT3=$1
+    docker run -p $1:5000 -d --name ${MODE}3 mjbright/flask-${MODE}:v3
+    shift
 
     #docker inspect ${MODE}1 | grep -i address
     IP1=$( docker inspect ${MODE}1 | grep IPAddress | tail -1 | sed -e 's/.*: "//' -e 's/",//' )
@@ -19,9 +25,9 @@ DOCKER_RUN() {
     IP3=$( docker inspect ${MODE}3 | grep IPAddress | tail -1 | sed -e 's/.*: "//' -e 's/",//' )
 
     echo "$MODE:"
-    echo "  Browse to http://$IP1:5000"
-    echo "  Browse to http://$IP2:5000"
-    echo "  Browse to http://$IP3:5000"
+    echo "  Browse to http://$IP1:5000 or http://127.0.0.1:$PORT1"
+    echo "  Browse to http://$IP2:5000 or http://127.0.0.1:$PORT2"
+    echo "  Browse to http://$IP3:5000 or http://127.0.0.1:$PORT3"
 }
 
 #MODE=store
@@ -35,8 +41,8 @@ if [ "$1" = "-rm" ]; then
     exit
 fi
 
-DOCKER_RUN store
-DOCKER_RUN quiz
-DOCKER_RUN survey
+DOCKER_RUN store   5001 5002 5003
+DOCKER_RUN quiz    6001 6002 6003
+DOCKER_RUN survey  7001 7002 7003
 
 
